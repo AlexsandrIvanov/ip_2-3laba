081428bd7c1f6afaa73eb5f356c8ebae61ff936c
AlexsandrIvanov <98846134+AlexsandrIvanov@users.noreply.github.com>
2022-05-20 15:46:42 +0400

    лаба 2-3 по ип, Иванов Александр, ИВТАСбд-12
    2я и 3я лаба по интернет программированию

---
+++ alexsandr/выполненные работы.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link  rel = "stylesheet"  href= "/style.css" >
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<body bgcolor="white">
+    
+    <tr>
+    <div class="panelnav">
+            <a href="главная.php"><font size=5>Главная</font></a></td>
+            <a href="о себе.php"><font size=5>О себе</font></a></td>
+            <a href="мои фотографии.php"><font size=5>мои фотографии</font></a></td>
+            <a href="галерея.php"><font size=5>Галерея</font></a></td>
+            <a href="выполненные работы.php"><font size=5>мои лабы</font></a></td>
+            <div class="avtorisator">
+                <a href="enter.php"><font size=5>Вход</font></a></td>
+                <a href="registration.php"><font size=5>Регистрация</font></a></td>
+            </div>
+    </div>
+    </tr>
+    <h1 class=mylabs> Мои лабораторнаые работы </h1>
+<div class=labs>
+    <a href="mldmlab1.php">Первая лабораторная работа по МЛиДМ</a> <br>
+    <a href="mldmlab1.php">Вторая лабораторная работа по МЛиДМ</a> <br>
+    <a href="mldmlab1.php">третья лабораторная работа по МЛиДМ</a> <br>
+    <a href="mldmlab1.php">четвертая лабораторная работа по МЛиДМ</a> <br>
+    <a href="mldmlab1.php">пятая лабораторная работа по МЛиДМ</a> <br>
+</div>
+
+
+</body>
+</html>
